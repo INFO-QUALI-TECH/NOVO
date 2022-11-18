c180b7993b04c9eb49151353ff608168779f8d73
João Pedro <107947773+joaopedromribeiro13@users.noreply.github.com>
2022-11-17 22:20:37 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('cadastro', 'Jp', 'galo1313', {
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+sequelize.authenticate().then(function(){
+    console.log('conectado com sucesso!')
+}).catch(function(erro){
+    console.log('falha ao se conectar: '+erro)
+})
